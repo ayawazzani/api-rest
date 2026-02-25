014e1bd901ad86cb19e4b8f466e949524cdec773
ayawazzani <2005120400095@ofppt-edu.ma>
2026-02-25 09:11:25 +0000

    modifier'

--- app.js
+++ app.js
@@ -5,24 +5,19 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const config = require('./config');
-
 // déclarer le router
 let CalculRouter = express.Router();
-
 // middlewares
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
-
 // CORS
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-
-
 // Route Somme
 CalculRouter.route('/somme')
 .post((req, res) => {
@@ -38,8 +33,6 @@ CalculRouter.route('/somme')
 
     res.json(success("La somme de " + nb1 + " et " + nb2 + " est : " + r));
 });
-
-
 // Route Produit
 CalculRouter.route('/produit')
 .post((req, res) => {
@@ -55,12 +48,10 @@ CalculRouter.route('/produit')
 
     res.json(success("Le produit de " + nb1 + " et " + nb2 + " est : " + r));
 });
-
-
 // route principale
 app.use(config.rootAPI + 'calculs', CalculRouter);
 
-
-// démarrer le serveur
-// في الأخير فقط
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
 module.exports = app;
